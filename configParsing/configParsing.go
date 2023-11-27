05840e89fb8e65d2aacc3958cebff012db9b6641
Maxime Moreillon <m.moreillon@gmail.com>
2023-11-28 07:41:15 +0900

    file splitting

---
+++ configParsing/configParsing.go
@@ -0,0 +1,32 @@
+package configparsing
+
+import (
+	"os"
+	"path/filepath"
+
+	"gopkg.in/yaml.v3"
+)
+
+
+type Config []struct {
+	Path string
+	Target string
+}
+
+
+func ParseConfigFile () Config {
+	filename, _ := filepath.Abs("./config/config.yml")
+	yamlFile, err := os.ReadFile(filename)
+	if err != nil {
+		panic(err)
+	}
+	
+	config := Config{}
+	err = yaml.Unmarshal(yamlFile, &config)
+	if err != nil {
+			panic(err)
+	}
+
+	return config
+}
+	
--- main.go
+++ main.go
@@ -3,52 +3,12 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"os"
-	"time"
-	"strconv"
-	"path/filepath"
-	"github.com/prometheus/client_golang/prometheus"
+	configparsing "redirection/configParsing"
+	"redirection/instrumentation"
+
 	"github.com/prometheus/client_golang/prometheus/promhttp"
-	"gopkg.in/yaml.v3"
 )
 
-type Config []struct {
-	Path string
-	Target string
-}
-
-// TODO: file splitting
-//statusRecorder to record the status code from the ResponseWriter
-type statusRecorder struct {
-	http.ResponseWriter
-	statusCode int
-}
-
-func measureResponseDuration(next http.Handler) http.Handler {
-	buckets := []float64{.005, .01, .025, .05, .1, .25, .5, 1, 2.5, 5, 10}
-
-	responseTimeHistogram := prometheus.NewHistogramVec(prometheus.HistogramOpts{
-		// Namespace: "namespace",
-		Name:      "http_request_duration_seconds",
-		Help:      "Histogram of response time for handler in seconds",
-		Buckets:   buckets,
-	}, []string{"route", "method", "status_code"})
-
-	prometheus.MustRegister(responseTimeHistogram)
-
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-
-		start := time.Now()
-		rec := statusRecorder{w, 200}
-		
-		next.ServeHTTP(&rec, r)
-
-		duration := time.Since(start)
-		statusCode := strconv.Itoa(rec.statusCode)
-		route := r.URL.Path
-		responseTimeHistogram.WithLabelValues(route, r.Method, statusCode).Observe(duration.Seconds())
-	})
-}
 
 
 func main() {
@@ -59,20 +19,10 @@ func main() {
 
 	// Sub Mux for redirection requests, metrics logged with Prometheus
 	subMux := http.NewServeMux()
-	wrappedMux := measureResponseDuration(subMux)
+	wrappedMux := instrumentation.MeasureResponseDuration(subMux)
 	
-	// Parse Yaml
-	filename, _ := filepath.Abs("./config/config.yml")
-	yamlFile, err := os.ReadFile(filename)
-	if err != nil {
-		panic(err)
-	}
 	
-	config := Config{}
-	err = yaml.Unmarshal(yamlFile, &config)
-	if err != nil {
-			panic(err)
-	}
+	config := configparsing.ParseConfigFile()
 
 	for _, config := range config {
 		fmt.Println(fmt.Sprintf("Registering redirection for %s to %s", config.Path, config.Target))
