627d553a428c8e632c79b903636080e5aef3087b
Maxime Moreillon <m.moreillon@gmail.com>
2023-11-29 05:31:01 +0900

    minor

--- main.go
+++ main.go
@@ -14,29 +14,29 @@ import (
 
 func main() {
 	
+	config := configparsing.ParseConfigFile()
+
 	// Main mux, serving metrics and subMux
 	mux := http.NewServeMux()
 	mux.Handle("/metrics", promhttp.Handler())
 
 	// Sub Mux for redirection requests, metrics logged with Prometheus
 	subMux := http.NewServeMux()
-	wrappedMux := instrumentation.MeasureResponseDuration(subMux)
-	
-	
-	config := configparsing.ParseConfigFile()
-
 	for _, config := range config {
 		fmt.Println(fmt.Sprintf("Registering redirection for %s to %s", config.Path, config.Target))
 
 		path := config.Path
 		target := config.Target
 
+		// Not Instrumented mux because otherwise multiple registration
 		subMux.Handle(path, http.RedirectHandler(target, 307))
 	}
 
-	fmt.Println("Redirection service listening on port :7070")
-	mux.Handle("/", wrappedMux)
+	instrumentedMux := instrumentation.MeasureResponseDuration(subMux)
+	mux.Handle("/", instrumentedMux)
 	corsHandler := cors.Default().Handler(mux)
+
 	http.ListenAndServe(":7070", corsHandler)
+	fmt.Println("Redirection service listening on port :7070")
 	
 }
